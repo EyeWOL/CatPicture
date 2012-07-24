7632a17cc7c33825b4ad141c1506248223891be6
Bo Brinkman <Bo.Brinkman@muohio.edu>
2012-07-24 15:38:27 -0400

    Draw some animated stripes in the surface.

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -29,8 +29,11 @@ class CatPictureApp : public AppBasic {
 	void draw();
 	void prepareSettings(Settings* settings);
 
-	Surface mySurface;
-	gl::Texture myTexture;
+	Surface mySurface_;
+	gl::Texture myTexture_;
+
+	///Number of times update has been called
+	int frameNumber_; 
 };
 
 void CatPictureApp::prepareSettings(Settings* settings){
@@ -40,18 +43,40 @@ void CatPictureApp::prepareSettings(Settings* settings){
 
 void CatPictureApp::setup()
 {
-	myTexture = gl::Texture(TEXTURE_WIDTH,TEXTURE_HEIGHT);
+	mySurface_ = Surface(TEXTURE_WIDTH,TEXTURE_HEIGHT,false);
+	frameNumber_ = 0;
 }
 
 void CatPictureApp::update()
 {
+	frameNumber_++;
 
+	uint8_t* dataArray = mySurface_.getData();
+
+	for(int i=0; i< TEXTURE_WIDTH*TEXTURE_HEIGHT*3; i++){
+		int row = i/(3*TEXTURE_WIDTH);
+		int col = (i/3) % TEXTURE_WIDTH;
+
+		int whichComponent = i % 3;
+
+		if(whichComponent == 0){
+			//If red, set to 0, otherwise make stripes
+			dataArray[i] = 0;
+		}else if((row+col+frameNumber_)/10 % 2 == 0){
+			dataArray[i] = 168;
+		} else {
+			dataArray[i] = 84;
+		}
+
+	}
+
+	myTexture_ = gl::Texture(mySurface_);
 }
 
 void CatPictureApp::draw()
 {
 	//Draw our texture to the screen, using graphics library
-	gl::draw(myTexture);
+	gl::draw(myTexture_);
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
