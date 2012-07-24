39bffb619b8a817c7f73093e32823950981f1666
Bo Brinkman <Bo.Brinkman@muohio.edu>
2012-07-24 13:47:39 -0400

    Added copyright info

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -4,6 +4,11 @@
  *
  * @author Bo Brinkman
  * @date 2012-07-24
+ *
+ * @note This file is (c) 2012. It is licensed under the 
+ * CC BY 3.0 license (http://creativecommons.org/licenses/by/3.0/),
+ * which means you are free to use, share, and remix it as long as you
+ * give attribution. Commercial uses are allowed.
  */
 
 #include "cinder/app/AppBasic.h"
